78ebcd8ee507b148fdd6de2443a3f2f0071f0f67
Nikita Petrenko <npetrenko97@gmail.com>
2019-08-09 16:01:52 +0300

    replaced old-style casts with literals

--- include/thread_pool/for_loop.hpp
+++ include/thread_pool/for_loop.hpp
@@ -5,6 +5,16 @@
 
 #include "future.hpp"
 
+namespace pfor_detail {
+uint8_t operator"" _u8t(unsigned long long value) {
+    return static_cast<uint8_t>(value);
+}
+
+size_t operator"" _sizet(unsigned long long value) {
+    return static_cast<size_t>(value);
+}
+}  // namespace pfor_detail
+
 class ParallelFor {
 public:
     ParallelFor(size_t from, size_t to, uint8_t step_complexity,
@@ -14,6 +24,7 @@ public:
 
     template <class FuncT>
     void operator()(FuncT func) && {
+        using namespace pfor_detail;
         // check if we are not running inside the executor
         if (NeedsFallback()) {
             FallbackLoop(std::move(func));
@@ -22,8 +33,8 @@ public:
 
         std::vector<std::shared_ptr<Task>> tasks;
 
-        size_t piece_size = (to_ - from_) / std::max(step_complexity_, (uint8_t)16);
-        piece_size = std::max((size_t)1, piece_size);
+        size_t piece_size = (to_ - from_) / std::max(step_complexity_, 16_u8t);
+        piece_size = std::max(1_sizet, piece_size);
 
         tasks.reserve(1 + ((to_ - from_) / piece_size));
 
@@ -57,9 +68,9 @@ private:
     }
 
     inline bool NeedsFallback() const {
-	if (step_complexity_ == 0) {
-	    return true;
-	}
+        if (step_complexity_ == 0) {
+            return true;
+        }
         auto tid = std::this_thread::get_id();
         if (exec_->GetWorkerThreadIds().count(tid)) {
             return true;
